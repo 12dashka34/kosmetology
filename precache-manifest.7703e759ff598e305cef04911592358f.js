1ead52026338bc3a953bb93c216e5d88b644fa0b
12dashka34 <12dashka34@gmail.com>
2024-01-16 23:25:25 +0200

    Create precache-manifest.7703e759ff598e305cef04911592358f.js

---
+++ precache-manifest.7703e759ff598e305cef04911592358f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e835c5c716ccfede5acdc872db357d60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3828d20b9985e283622c",
+    "url": "/static/css/main.f3dc357d.chunk.css"
+  },
+  {
+    "revision": "f253a59535c9e1e8abc7",
+    "url": "/static/js/2.f4002c45.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f4002c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3828d20b9985e283622c",
+    "url": "/static/js/main.993a70af.chunk.js"
+  },
+  {
+    "revision": "791ac080b92a6024407f",
+    "url": "/static/js/runtime-main.d577d004.js"
+  }
+]);
